d8deff14e01a5deecb10a87c857205a9b6cdf24a
ToxicScorpius <toxicscorpius@gmail.com>
2020-11-30 10:56:16 -0500

    Generator to read the file by line + timer

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 from itertools import zip_longest
+import timeit
 
-# Reads the file and assigns their value to a string
-file_1 = open("homo_sapiens_hbb.fasta", "r")
-file_2 = open("pan_troglodytes_hbb.fasta", "r")
-str_file_1 = file_1.read()
-str_file_2 = file_2.read()
+start = timeit.default_timer()
 
-# Deletes all line breaks
-str_file_1 = str_file_1.replace("\n", "")
-str_file_2 = str_file_2.replace("\n", "")
+# Opens the file, ignores the header and turns lines into iterables
+def iter_adn_seq(path):
+    with open(path) as file:
+        for line in file:
+            if line.startswith(">"):
+                continue
+            yield from line.strip()
 
-files_list = [str_file_1, str_file_2]
-
-# Defines the amount of matches and differences
-n_match, n_diff = 0, 0
-
-print(str_file_1)
-print(str_file_2)
+n_diff, n_match = 0, 0
 
 # Associates each character to one another and shows differences
 # Then counts the amount of matches and differences
-for n_iter, (c_str1, c_str2) in enumerate(zip_longest(str_file_1, str_file_2), 1):
+for n_iter, (c_str1, c_str2) in\
+        enumerate(zip_longest(iter_adn_seq("sars_cov2.fasta"), iter_adn_seq("sars_tor2.fasta")), 1):
     if c_str1 != c_str2:
         print(f"{n_iter}: {c_str1}, {c_str2}")
         n_diff += 1
@@ -31,6 +27,8 @@ for n_iter, (c_str1, c_str2) in enumerate(zip_longest(str_file_1, str_file_2), 1
 print(f"{n_diff} differences")
 
 # Displays the percentage of similarity, with a 2 digits precision
-longest_str = max(files_list, key=len)
-similarity = "{:.2f}".format(n_match * 100 / len(longest_str))
+similarity = "{:.2f}".format(n_match * 100 / n_iter)
 print(f"{similarity}% similar")
+
+stop = timeit.default_timer()
+print(f"Time: {stop - start}s")  
