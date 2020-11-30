d0dd4ec2077ed918ffd5778f41efc2e407c7b516
ToxicScorpius <toxicscorpius@gmail.com>
2020-11-29 23:20:08 -0500

    Supports the comparison between text files

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from itertools import zip_longest
-import random
-import string
 
-# Strings consist of selected characters
-letters = string.ascii_lowercase
+# Reads the file and assigns their value to a string
+file_1 = open("homo_sapiens_hbb.fasta", "r")
+file_2 = open("pan_troglodytes_hbb.fasta", "r")
+str_file_1 = file_1.read()
+str_file_2 = file_2.read()
 
-# Generates 2 strings of random length and characters
-string1 = "".join(random.choice(letters) for i in range(random.randint(1, 20)))
-string2 = "".join(random.choice(letters) for i in range(random.randint(1, 20)))
-# Makes a list with these strings
-list_str = [string1, string2]
+# Deletes all line breaks
+str_file_1 = str_file_1.replace("\n", "")
+str_file_2 = str_file_2.replace("\n", "")
+
+files_list = [str_file_1, str_file_2]
 
 # Defines the amount of matches and differences
 n_match, n_diff = 0, 0
 
-print(string1)
-print(string2)
+print(str_file_1)
+print(str_file_2)
 
 # Associates each character to one another and shows differences
 # Then counts the amount of matches and differences
-for n_iter, (c_str1, c_str2) in enumerate(zip_longest(string1, string2)):
+for n_iter, (c_str1, c_str2) in enumerate(zip_longest(str_file_1, str_file_2), 1):
     if c_str1 != c_str2:
         print(f"{n_iter}: {c_str1}, {c_str2}")
         n_diff += 1
@@ -29,7 +30,7 @@ for n_iter, (c_str1, c_str2) in enumerate(zip_longest(string1, string2)):
 # Displays the amount of differences
 print(f"{n_diff} differences")
 
-# Displays the percentage of similarity
-longest_str = max(list_str, key=len)
+# Displays the percentage of similarity, with a 2 digits precision
+longest_str = max(files_list, key=len)
 similarity = "{:.2f}".format(n_match * 100 / len(longest_str))
 print(f"{similarity}% similar")
